1a4332004d1988675a382c50e3a8ac3e268445a6
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-11-16 23:45:08 +0100

    samples

---
+++ samples/XamarinProjectsStructureTemplate.Sample.ConsoleApp.netcoreapp30/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XamarinProjectsStructureTemplate.Sample.ConsoleApp.netcoreapp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ samples/XamarinProjectsStructureTemplate.Sample.XamarinForms.Shell/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinProjectsStructureTemplate.Sample.XamarinForms.Shell.Models;
+
+namespace XamarinProjectsStructureTemplate.Sample.XamarinForms.Shell.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
