850088e202b321a8d7720a6e0631a7ed186cc8fe
moljac <mcvjetko@holisticware.net>
2019-11-24 21:19:18 +0100

    Xamarin.Mac app added

---
+++ samples/XamarinProjectsStructureTemplate.Sample.XamarinForms.Blank.XamarinMac/Main.cs
@@ -0,0 +1,16 @@
+﻿using AppKit;
+
+namespace XamarinProjectsStructureTemplate.Sample.XamarinForms.Blank
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
+            NSApplication.Main(args);
+
+            return;
+        }
+    }
+}
