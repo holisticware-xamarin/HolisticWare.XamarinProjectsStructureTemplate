55c345592bdcff68c8322dbef43c0816e10792dd
moljac <mcvjetko@holisticware.net>
2019-11-27 21:55:09 +0100

    samples

---
+++ samples/XamarinProjectsStructureTemplate.Sample.XamarinForms.Blank.GTK/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+namespace XamarinProjectsStructureTemplate.Sample.XamarinForms.Blank.GTK
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Xamarin.Forms.Forms.Init();
+
+            var app = new App();
+            var window = new Xamarin.Forms.Platform.GTK.FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("XamarinProjectsStructureTemplate.Sample.XamarinForms.Blank.GTK");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
